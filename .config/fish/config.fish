e65901f6fb3586cea4a449853b2be90c64e1178e
Derek Baker <drkbkr@users.noreply.github.com>
2021-10-15 11:31:59 -0400

    starship

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,7 @@ alias ts='tailscaled'
 alias tsu='tailscale up --accept-routes'
 alias tsd='tailscale down'
 alias tss='tailscale status'
+
+if status --is-interactive
+  source("/usr/bin/starship init fish --print-full-init | psub)
+end
